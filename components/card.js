80ffdf31558db1e250babd0861fd5ef043d5dd7c
James Furtado <jamesdfurtado@gmail.com>
2025-09-12 10:24:12 -0400

    feat: built out first 'card' component prototype

--- components/card.js
+++ components/card.js
@@ -1,7 +1,28 @@
 
-function createCard(title="title", desc="desc"){
-    console.log("running createCard!")
+function createCard(title="title", imgSrc=null, desc="desc"){
     const card = document.createElement("div");
     card.className = "card";
     document.body.append(card);
+
+    const titleEl = document.createElement("p");
+    titleEl.innerText = title;
+    card.append(titleEl);
+
+    const imageEl = document.createElement("img");
+    imageEl.setAttribute("src", imgSrc);
+    imageEl.setAttribute("alt", "photo loading error");
+    card.append(imageEl)
+
+    const descEl = document.createElement("p");
+    descEl.innerText = desc;
+    card.append(descEl);
 }
+
+/* HTML equivelant
+
+   <div class="card" onclick="goToPage('pages/mini-ecommerce')">
+        <p>Mini ecommerce SPA</p>
+        <img alt="photo"></img>
+        <p>A short description of the project.</p>
+    </div>
+*/
